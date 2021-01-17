0476a9e98b40b9d5b9a028d97034bc4a3263dbc5
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-17 21:03:08 +0700

    add

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        String s = " Hello Java ! ";
+//        System.out.println(s);
+
+        String t = "Dang Xuan Thao ";
+
+        String sumString = t + s;
+        System.out.println(sumString);
+
+        int i = 10;
+        System.out.println(sumString + i);
+    }
+}
